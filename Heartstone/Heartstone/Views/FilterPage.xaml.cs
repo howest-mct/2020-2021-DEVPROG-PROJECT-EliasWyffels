a524069b9bd09634b1256dde58d50253b45b8fd5
EliasWyffels <55881967+elias.wyffels@student.howest.be>
2020-11-21 16:26:50 +0100

    probleempje met emulator opgelost en classe mee gegeven naar 2de scherm

---
+++ Heartstone/Heartstone/Views/FilterPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Heartstone.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FilterPage : ContentPage
+    {
+        public FilterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(hunterlbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(warriorlbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(roguelbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_3(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(warriorlbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_4(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(paladinlbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_5(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(druidlbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_6(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(magelbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_7(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(priestlbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_8(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(shamanlbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_9(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards(neutrallbl.Text.ToLower()));
+        }
+
+        private void Button_Pressed_10(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Heartstone/Heartstone/Views/ShowCards.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Heartstone.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ShowCards : ContentPage
+    {
+        public ShowCards(string v)
+        {
+            InitializeComponent();
+            SetTitel(v);
+        }
+
+        private void SetTitel(string name)
+        {
+            titel.Text = name.ToUpper();
+
+            switch (name)
+            {
+                case "hunter":
+                    classcolor.BackgroundColor = Color.FromHex("#016E01");
+                    break;
+
+                case "warrior":
+                    classcolor.BackgroundColor = Color.FromHex("#8E1002");
+                    break;
+
+                case "priest":
+                    classcolor.BackgroundColor = Color.FromHex("#C7C19F");
+                    break;
+
+                case "mage":
+                    classcolor.BackgroundColor = Color.FromHex("#0092AB");
+                    break;
+
+                case "rogue":
+                    classcolor.BackgroundColor = Color.FromHex("#4C4D48");
+                    break;
+
+                case "druid":
+                    classcolor.BackgroundColor = Color.FromHex("#703606");
+                    break;
+
+                case "shaman":
+                    classcolor.BackgroundColor = Color.FromHex("#8E1002");
+                    break;
+
+                case "warlock":
+                    classcolor.BackgroundColor = Color.FromHex("#7624AD");
+                    break;
+
+                case "paladin":
+                    classcolor.BackgroundColor = Color.FromHex("#AA8F00");
+                    break;
+
+                case "neutral":
+                    classcolor.BackgroundColor = Color.LightGray;
+                    break;
+            }
+        }
+    }
+}
