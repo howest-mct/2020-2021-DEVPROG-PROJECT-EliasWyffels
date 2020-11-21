e94e4147dbfa858d7c3aaecd3aa8779f675d9e07
EliasWyffels <55881967+elias.wyffels@student.howest.be>
2020-11-21 11:34:29 +0100

    scherm 1 gemaakt

---
+++ ProjectHeartstone/ProjectHeartstone/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ProjectHeartstone.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectHeartstone
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new FilterPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ProjectHeartstone/ProjectHeartstone/Views/FilterPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjectHeartstone.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FilterPage : ContentPage
+    {
+        public FilterPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Pressed(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ShowCards());
+        }
+
+        private void Button_Pressed_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_5(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_6(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_7(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_8(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_9(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Pressed_10(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
